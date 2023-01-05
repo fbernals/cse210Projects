99c7ab413337d8b44402c168d1509a686c2cb17e
Fabian Bernal <fbernals@aol.com>
2023-01-04 17:41:40 -0700

    Prep4 Lists handling

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,63 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number = 0;
+        int total = 0;
+        double average = 0;
+        int maximum = 0;
+        int minimum = 0;
+        int aMinimum = 100;
+
+        do
+        {
+
+            Console.Write("Enter a number: ");
+            string userInput = Console.ReadLine();
+            number = int.Parse(userInput);
+            if (number != 0)
+            {
+                numbers.Add(number);
+
+            }else if (number == 0)
+            {
+                break;
+            }
+
+        }while(number != 0);
+
+        for (int i = 0; i < numbers.Count; i++)
+        {
+
+        Console.WriteLine(numbers[i]);
+        total = numbers.Sum();
+        average = numbers.Average();
+        maximum = numbers.Max();
+        if (numbers[i] > 0)
+        {
+            minimum = numbers[i];
+            if (minimum < aMinimum)
+            {
+                aMinimum = minimum;
+            }
+
+        }
+
+        }
+
+        numbers.Sort();
+
+        Console.WriteLine($"The sum of all list items is {total}");
+        Console.WriteLine($"The average of all items is {average}");
+        Console.WriteLine($"The maximum value is {maximum}");
+        Console.WriteLine($"The minimum positive value is {aMinimum}");
+        Console.WriteLine("This is the sorted list:");
+        foreach(int item in numbers)
+        {
+            Console.WriteLine(item);
+        }
+
     }
+    
+    
 }
