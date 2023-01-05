46daecf80c446487cf851dced57fa60f681b0bd5
Fabian Bernal <fbernals@aol.com>
2023-01-05 06:48:07 -0700

    Prep 5, functions

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome(); //Dsiplays a welcome message.
+        string uName = GetUserName(); //Calls a function to prompt for user name.
+        int number = GetUserNumber(); //Prompts for a number.
+        int square = SquareNumber(number); //Squares the number received.
+        DisplayInfo(uName, square); //DIsplays the info received and processed.    
+
     }
+    static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the machine!");
+        }
+
+    static string GetUserName()
+    {
+        //Prompts for and returns user name.
+        Console.Write("What is your last name? ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+    static int GetUserNumber()
+    {   
+        //Prompts for and returns a number
+        Console.Write("Tel me your favorite number: ");
+        string userInput = Console.ReadLine();
+        int userNumber = int.Parse(userInput);
+        return userNumber;
+    }
+    static int SquareNumber(int number)
+    {
+        //Squares a number received.
+        int squared = number*number;
+        return squared;
+    }
+
+    static void DisplayInfo(string uName, int square)
+    {
+        //Displays processed info.
+        Console.WriteLine($"Brother {uName}, the square of your number is {square}.");
+    }
+    
+    
 }
